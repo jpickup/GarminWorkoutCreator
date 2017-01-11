96c02f9e01d100d14a4b224686e0f3b9923f7b06
John Pickup <home@johnpickup.com>
2017-01-11 20:41:09 +0000

    Excel reader - add workouts

--- src/com/johnpickup/excel/ExcelWorkoutScheduleReader.java
+++ src/com/johnpickup/excel/ExcelWorkoutScheduleReader.java
@@ -2,7 +2,6 @@ package com.johnpickup.excel;
 
 import com.johnpickup.parser.WorkoutSchedule;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 
 import java.io.FileInputStream;
@@ -10,18 +9,20 @@ import java.io.IOException;
 
 
 /**
- * Created by john on 10/01/2017.
+ * Class to read all the content of a Workout Schedule Excel workbook
  */
 public class ExcelWorkoutScheduleReader {
     private PaceSheetReader paceReader = new PaceSheetReader();
+    private WorkoutSheetReader workoutReader = new WorkoutSheetReader();
 
     public WorkoutSchedule read(String filename) throws IOException {
         WorkoutSchedule result = new WorkoutSchedule();
         FileInputStream inputFile = new FileInputStream(filename);
 
         Workbook wb = new HSSFWorkbook(inputFile);
-        Sheet paceSheet = wb.getSheet("Pace");
-        result.getPaces().putAll(paceReader.readPaces(paceSheet));
+        result.getPaces().putAll(paceReader.readPaces(wb.getSheet("Pace")));
+        result.getWorkouts().putAll(workoutReader.readWorkouts(wb.getSheet("Workout")));
+
         wb.close();
 
         return result;
--- test/com/johnpickup/excel/ExcelWorkoutScheduleReaderTest.java
+++ test/com/johnpickup/excel/ExcelWorkoutScheduleReaderTest.java
@@ -3,6 +3,10 @@ package com.johnpickup.excel;
 import com.johnpickup.parser.*;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
@@ -22,6 +26,17 @@ public class ExcelWorkoutScheduleReaderTest {
         expected.getPaces().put("Easy", new PaceRange(new Time(9,0), new Time(10,30), PaceUnit.MIN_PER_MILE));
         expected.getPaces().put("Slow", new PaceRange(new Time(10,0), new Time(15,0), PaceUnit.MIN_PER_MILE));
 
+        expected.getWorkouts().put("5mi Slow", new Workout(Collections.singletonList(new PaceStep(new Distance(5, DistanceUnit.MILE), new PaceName("Slow")))));
+        List<Step> intervalSteps = new ArrayList<>();
+        intervalSteps.add(new DistanceStep(new Distance(1, DistanceUnit.MILE)));
+        RepeatingSteps repeatingSteps = new RepeatingSteps(new PaceStep(new Distance(1, DistanceUnit.MILE), new PaceName("Fast")));
+        repeatingSteps.addStep(new PaceStep(new Distance(400, DistanceUnit.METRE), new PaceName("Easy")));
+        repeatingSteps.setRepetitions(4);
+        intervalSteps.add(repeatingSteps);
+        intervalSteps.add(new DistanceStep(new Distance(1, DistanceUnit.MILE)));
+        expected.getWorkouts().put("4x1mi Interval", new Workout(intervalSteps));
+        expected.getWorkouts().put("6mi Steady", new Workout(Collections.singletonList(new PaceStep(new Distance(6, DistanceUnit.MILE), new PaceName("Steady")))));
+
         assertEquals(expected, actual);
     }
 
@@ -34,4 +49,10 @@ Brisk	7:00-8:00/mi
 Steady	07:30-08:30/mi
 Easy	09:00-10:30/mi
 Slow	10:00-15:00/mi
+
+WORKOUT TEST DATA
+Name	Description
+5mi Slow	5mi@Slow
+4x1mi Interval	1mi + (1mi@Fast + 400m@Easy)*4 + 1mi
+6mi Steady	6mi@Steady
 */
